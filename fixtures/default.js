dd9ff06c5f21ed22543a9055411471cc05acf3da
Justin Fargione <justin@lexicalbits.com>
2015-09-04 16:40:22 -0400

    Changes required by "collections everywhere", as well as improved handling of multiple kinds of Slack output

--- fixtures/default.js
+++ fixtures/default.js
@@ -1,25 +1,54 @@
-var fs = require('fs');
+var _ = require('lodash')
+    , env = require('./env')
+    ;
 
-module.exports = {
-    "input": {
-        "messages": JSON.parse(fs.readFileSync(__dirname + '/messages.json')).channel_history
-    },
-    "privates": {
-       /*
-        * e.g. oauth keys, slack tokens
-        * "instapaper_consumer_key": "somerandomcharacters",
-        * "instapaper_consumer_secret": "somemorerandomcharacters"
-        */
-    },
-    "providers": {
-       "instapaper": {
-          "access_token": "{\"oauth_token_secret\":\"providerdatacapturedydexter\",\"oauth_token\":\"moreproviderdatacapturedbydexter\"}"
-       }
-    }, 
-    "settings": {
-       /*
-        * settings data passed to the step
-        * "mustache": "A sample {mustache} template"
-        */
+module.exports = _.merge({
+    simulation: true
+    , instance_id : 'local_test_instance'
+    , urls: {
+        home : "http://rundexter.com/"
     }
-};
+    , instance_state: {
+        active_step :  "local_test_step"
+    }
+    , workflow: {
+        "id"            : "local_test_workflow"
+        , "title"       : "Local test workflow"
+        , "description" : "A fixture workflow used to test a module"
+    }
+    , steps: {
+        local_test_step: {
+            id: 'local_test_step'
+            , type: 'module'
+            //The test runner will change YOUR_MODULE_NAME to the correct module name
+            , name: 'YOUR_MODULE_NAME'
+            , next: []
+        }
+    }
+    , modules: {
+    }
+    , data: {
+        local_test_step: {
+            input: {
+                text: [
+                    'Nothing to see in http!'
+                    , 'I didn\'t find anything.'
+                    , 'I found the answer on http://google.com'
+                    , 'Google now knows more about me than my http://parents.com!'
+                ],
+                attachment: [null, {
+                    "title": "(Super Low-Tech) Apple Watch",
+                    "title_link": "http://www.hinemizushima.com/72050/6059931/felt-sculptures/apple-watch",
+                    "text": "Yes, I already got it.",
+                    "fallback": "(Super Low-Tech) Apple Watch",
+                    "image_url": "https://mir-cdn.behance.net/v1/rendition/projects/202/467b0a24375579.55164f27b2496.jpg",
+                    "from_url": "http://www.hinemizushima.com/72050/6059931/felt-sculptures/apple-watch",
+                    "image_width": 202,
+                    "image_height": 158,
+                    "image_bytes": 16438,
+                    "id": 1
+                }, null, null]
+            }
+        }
+    }
+}, env);
--- index.js
+++ index.js
@@ -1,26 +1,57 @@
-var _ = require('lodash');
+var _ = require('lodash')
+    //From:
+    //http://stackoverflow.com/a/6041965
+    , reTest = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/
+    ;
 
 module.exports = {
     init: function() {
     }
     , run: function(step, dexter) {
-        var messages = step.input('messages')
-          , lastMatch 
-          , msg = messages && messages.length 
-             ? _.find(messages, function(msg) {
-                  return (lastMatch = msg.text.match(/<(http.+?)>/));
-               })
-             : null
+        var text = step.input('text')
+          , attachments = step.input('attachment')
+          , items = text.length || attachments.length
+          , toTest = []
+          , match
+          , i
         ; 
 
-        if(!messages || !messages.length) {
-            return this.fail({ message: 'No Messages Found' });
+        if(text.length !== 0 && attachments.length !== 0
+           && text.length != attachments.length) {
+               return this.fail('If you pass both text and attachments, they must be the same length');
+        }
+        for(i = 0; i < items; i ++) {
+            toTest.push([]);
+            if(attachments[i]) {
+                if(attachments[i].title_link) {
+                    toTest[i].push(attachments[i].title_link);
+                    //No need to go further with this index, since we KNOW this is a link
+                    continue;
+                }
+                if(attachments[i].text) {
+                    toTest[i].push(attachments[i].text);
+                }
+            }
+            if(text[i]) {
+                toTest[i].push(text[i]);
+            }
         }
 
-        this.log('MESSAGES RECEIVED', messages);
-
-        return this.complete({
-            url: msg ? lastMatch[1].split('|')[0] : null
+        _.each(toTest, function(tests) {
+            _.each(tests, function(test) {
+                match = test.match(reTest);
+                if(match) {
+                    match = match[0];
+                    return false;
+                }
+            });
+            if(match) {
+                return false;
+            }
         });
+        if(match) {
+            this.complete({ url: match });
+            return false;
+        }
     }
 };
